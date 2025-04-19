97a85df8b19b44e6b0774d1d8f7980daa34daf42
Sauhard Semalty <186826343+Sauhard56@users.noreply.github.com>
2025-04-19 17:42:46 +0530

    fix: `on_close` not working on `destroy()`

--- remchap_client/ui/toplevel_base.py
+++ remchap_client/ui/toplevel_base.py
@@ -6,3 +6,5 @@ import customtkinter
 class TopLevelBase(customtkinter.CTkToplevel):
     def on_close(self, cb: Callable):
         self.protocol("WM_DELETE_WINDOW", cb)
+
+        self.bind("<Destroy>", lambda e: cb() if e.widget == self else None)
